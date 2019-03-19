760993544594c2c4b4c05760a6ddeb5392b760e3
Alexandra Lisicyna <miss.lisicyna@gmail.com>
2019-03-19 21:31:24 +0300

    Find r''

--- iteration.py
+++ iteration.py
@@ -58,47 +58,61 @@ def findsolve(r):
     #print("solve2 for %f = %f" %(x, findsolve2(x)))
     #print("\n")
 
-r = 0.1
-i = 0
-f = open("solve1.txt", "w")
-x0 = 0.5
-f.write("%d %.5f\n" %(i, x0))
-i += 1
-x1 = r * x0 * (1 - x0)
-f.write("%d %.5f\n" %(i, x1))
-i += 1
-while abs(x1 - x0) > 0.00000000001:
-    x0 = x1
-    x1 = r * x0 * (1 - x0)
-    f.write("%d %.5f\n" % (i, x1))
-    i += 1
+# r = 0.1
+# i = 0
+# f = open("solve1.txt", "w")
+# x0 = 0.5
+# f.write("%d %.5f\n" %(i, x0))
+# i += 1
+# x1 = r * x0 * (1 - x0)
+# f.write("%d %.5f\n" %(i, x1))
+# i += 1
+# while abs(x1 - x0) > 0.00000000001:
+#     x0 = x1
+#     x1 = r * x0 * (1 - x0)
+#     f.write("%d %.5f\n" % (i, x1))
+#     i += 1
+#
+# r = 1.05
+# i = 0
+# f = open("solve2.txt", "w")
+# x0 = 0.5
+# f.write("%d %.5f\n" %(i, x0))
+# i += 1
+# x1 = r * x0 * (1 - x0)
+# f.write("%d %.5f\n" %(i, x1))
+# i += 1
+# while abs(x1 - x0) > 0.00000000001:
+#     x0 = x1
+#     x1 = r * x0 * (1 - x0)
+#     f.write("%d %.5f\n" % (i, x1))
+#     i += 1
+#
+# r = 2.1
+# i = 0
+# f = open("solve3.txt", "w")
+# x0 = 0.5
+# f.write("%d %.5f\n" %(i, x0))
+# i += 1
+# x1 = r * x0 * (1 - x0)
+# f.write("%d %.5f\n" %(i, x1))
+# i += 1
+# while abs(x1 - x0) > 0.00000000001:
+#     x0 = x1
+#     x1 = r * x0 * (1 - x0)
+#     f.write("%d %.5f\n" % (i, x1))
+#     i += 1
 
-r = 1.05
-i = 0
-f = open("solve2.txt", "w")
-x0 = 0.5
-f.write("%d %.5f\n" %(i, x0))
-i += 1
-x1 = r * x0 * (1 - x0)
-f.write("%d %.5f\n" %(i, x1))
-i += 1
-while abs(x1 - x0) > 0.00000000001:
-    x0 = x1
-    x1 = r * x0 * (1 - x0)
-    f.write("%d %.5f\n" % (i, x1))
-    i += 1
 
-r = 2.1
-i = 0
-f = open("solve3.txt", "w")
-x0 = 0.5
-f.write("%d %.5f\n" %(i, x0))
-i += 1
-x1 = r * x0 * (1 - x0)
-f.write("%d %.5f\n" %(i, x1))
-i += 1
-while abs(x1 - x0) > 0.00000000001:
-    x0 = x1
-    x1 = r * x0 * (1 - x0)
-    f.write("%d %.5f\n" % (i, x1))
-    i += 1
+left = 1.0
+right = 3.0
+while abs(left - right) > 0.00000000001:
+    mid = (left + right) / 2
+    x = findsolve(mid)
+    x0 = 0.5
+    x1 = mid * x0 * (1 - x0)
+    if (x - x0) * (x - x1) < 0:
+        right = mid
+    else:
+        left = mid
+print(left)
